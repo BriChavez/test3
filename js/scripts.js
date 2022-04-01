d98214f0e7aeca314d44e3a8d1ec155e537b0ae8
BriChavez <brianachavez@gmail.com>
2022-04-01 15:54:13 -0700

    add colors and other pretty things

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,11 @@
+$(document).ready(function() {
 
-
-
-  
- 
- 
- 
-
-
-// function boop(number) {
-//   for (let index = 0; index <= number; index++); {
-//     array.push(index);
-//   }
-//   return array;
-// }
-
-//  for (let index = 0; index <= input; index++)
-
-
-
-// function translator(input) {
-//   for (let index = 0; index <= input; index++); {
-  
-//     if(index.toString().includes(3)) {
-//       numArray[index] = "Won't you be my neighbor?"; 
-//       } else if (index.toString().includes(2)) {
-//         numArray[index] = "Boop!";
-//       }
-//       else if (index.toString().includes(1)) {
-//         numArray[indext] = "Beep";
-//         }
-//       else {
-//         numArray[index] = indext.toString();
-//       } 
-//       return numArray;
-//   }
-// }
-
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    const output = $("#input").val();
+    $("#output").text(translator(output));
+  });
+})
 
 function translator(number) {
   const numArray = [];
@@ -54,13 +24,4 @@ function translator(number) {
     } 
   }
   return numArray;
-} 
-
-$(document).ready(function() {
-
-  $("#userInput").submit(function(event) {
-    event.preventDefault();
-    const output = $("#input").val();
-    $("#output").text(translator(output));
-  });
-})
+}
