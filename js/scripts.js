2f82780e71b1ce3a6c4353e9090c52d33b1c2c03
BriChavez <brianachavez@gmail.com>
2022-04-01 12:11:49 -0700

    recognize a 2 and turn it into a boop

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,14 @@
   
  
  let numArray = [];
-  for (let index = 0; index <= input; index ++) {
-    console.log(index);
-  
-  }
 
-  for (let index = 0; index <= number; index++) {
+
+  for (let index = 0; index <= input; index++) {
     if(index.toString().includes(3)) {
       numArray[index] = "Won't you be my neighbor?";
       console.log(numArray);
+    } else if (index.toString().includes(2)) {
+      numArray[index] = "Boop!";
   } 
 
   }
